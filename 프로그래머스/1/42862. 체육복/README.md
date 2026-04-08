9186a8f415049638793094f4da12ee87a2f69c2f
Sunha Park <103306888+sunha21@users.noreply.github.com>
2026-04-08 22:05:44 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,6 +1,6 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 08일 22:05:33
+2026년 04월 08일 22:05:41
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,26 +1,3 @@
 def solution(n, lost, reserve):
-    lost_pe = 0    # 빌린 애들
-    reserve_pe = len(reserve) # 일단 얘네는 체육을 할 수 있기는 한 애들
-    own = n - len(set(lost + reserve)) # 그냥 딱 자기꺼 하나 있는애들
-    lost2 = lost[:]
-    
-    reserve.sort()
-    lost.sort()
-
-    
-    for x in lost2:
-        if x in reserve:
-            reserve.remove(x)
-            lost.remove(x)
-    
-    for x in lost:
-        if x - 1 in reserve:
-            lost_pe += 1
-            reserve.remove(x-1)
-        
-        elif x + 1 in reserve:
-            lost_pe += 1
-            reserve.remove(x+1)
-            
-    answer = reserve_pe + lost_pe + own
+    answer = 0
     return answer
