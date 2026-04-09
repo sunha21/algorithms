afdaf4fa24477e1076cf6b464c75b921a64991b4
Sunha Park <103306888+sunha21@users.noreply.github.com>
2026-04-09 20:28:08 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.38 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,15 +1,4 @@
 def solution(price, money, count):
-    
-    money_need = 0
-    
-    for i in range(count):
-        n = i + 1
-        money_need += price * n
-        
+    answer = -1
 
-    answer = money_need - money
-    
-    if answer < 0:
-        return 0
-    
     return answer
